b71691e9ee75cb33ba4ef7110b29a556de5cfec1
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-12-08 19:26:05 -0300

    refactor: muzzle flash vfx script cleaned.

--- GDTV_Multiplayer_Course_Project/Assets/Scripts/VFX/MuzzleFlashVfx.cs
+++ GDTV_Multiplayer_Course_Project/Assets/Scripts/VFX/MuzzleFlashVfx.cs
@@ -8,66 +8,23 @@ public class MuzzleFlashVfx : NetworkBehaviour
     [SerializeField] ProjectileLauncher _projectileLauncher = null;
     [SerializeField] SpriteRenderer _renderer = null;
     [SerializeField] float _duration = 0.1f;
-    private float _timer = 0f;
 
-    public override void OnNetworkSpawn()
+    private void OnEnable()
     {
-        base.OnNetworkSpawn();
         _renderer.enabled = false;
-        //if (!IsOwner) return;
         _projectileLauncher.OnShoot += PlayAll;
-        //_projectileLauncher.OnShoot += Fodase;
-        //_projectileLauncher.OnShoot += PlayClientRpc;
-        //_projectileLauncher.OnShoot += PlayServerRpc;
     }
 
-    public override void OnNetworkDespawn()
+    private void OnDisable()
     {
-        base.OnNetworkDespawn();
-        //if (!IsOwner) return;
         _projectileLauncher.OnShoot -= PlayAll;
-        //_projectileLauncher.OnShoot -= Fodase;
-        //_projectileLauncher.OnShoot -= PlayClientRpc;
-        //_projectileLauncher.OnShoot -= PlayServerRpc;
     }
-
-    //private void Update()
-    //{
-    //    _timer += Time.deltaTime;
-
-    //    if (_timer > _duration && _renderer.enabled)
-    //    {
-    //        _renderer.enabled = false;
-    //    }
-    //}
 
     private void PlayAll()
     {
         Play();
-        PlayServerRpc();
-        PlayClientRpc();
-    }
-
-    public void Fodase()
-    {
-        _timer = 0f;
-        _renderer.enabled = true;
-        //PlayServerRpc();
-        //PlayClientRpc();
-    }
-
-    [ServerRpc]
-    public void PlayServerRpc()
-    {
-        //Fodase();
-        Play();
-    }
-
-    [ClientRpc]
-    private void PlayClientRpc()
-    {
-        //Fodase();
-        Play();
+        Play_ServerRpc();
+        Play_ClientRpc();
     }
 
     private void Play()
@@ -81,5 +38,17 @@ public class MuzzleFlashVfx : NetworkBehaviour
     {
         yield return new WaitForSeconds(_duration);
         _renderer.enabled = false;
+    }
+
+    [ServerRpc]
+    public void Play_ServerRpc()
+    {
+        Play();
+    }
+
+    [ClientRpc]
+    private void Play_ClientRpc()
+    {
+        Play();
     }
 }
