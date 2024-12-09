76b7476d94a2c34919ab4e98e128658ed0e237e9
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-12-09 19:20:49 -0300

    test: color randomizer test.

--- GDTV_Multiplayer_Course_Project/Assets/Scripts/VFX/ColorRandomizer.cs
+++ GDTV_Multiplayer_Course_Project/Assets/Scripts/VFX/ColorRandomizer.cs
@@ -7,33 +7,75 @@ public class ColorRandomizer : NetworkBehaviour
 {
     [SerializeField] SpriteRenderer[] _sprites = null;
 
-    public override void OnNetworkSpawn()
+    private Color _color = default;
+
+    //public override void OnNetworkSpawn()
+    //{
+    //    base.OnNetworkSpawn();
+    //    //ChangeColor();
+    //    //ChangeColorClientRpc();
+
+    //    //if (!IsOwner) return;
+    //    //PickColor();
+    //    //SetSprites();
+    //    //SetSprites_ServerRpc();
+    //    //SetSprites_ClientRpc();
+
+    //    Invoke(nameof(Fodase), 20f);
+    //}
+
+    private void Fodase()
     {
-        base.OnNetworkSpawn();
-        //ChangeColor();
-        //ChangeColorClientRpc();
+        if (!IsOwner) return;
+
+        PickColor();
+        SetSprites();
+        SetSprites_ServerRpc();
+        SetSprites_ClientRpc();
     }
 
-    //private void Awake()
+    private void PickColor()
+    {
+        _color = Random.ColorHSV(0, 1, 0, 1, 0.5f, 1, 1, 1);
+    }
+
+    private void SetSprites()
+    {
+        var _count = _sprites.Length;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _sprites[i].color = _color;
+        }
+    }
+
+    [ServerRpc]
+    private void SetSprites_ServerRpc()
+    {
+        SetSprites();
+    }
+
+    [ClientRpc]
+    private void SetSprites_ClientRpc()
+    {
+        SetSprites();
+    }
+
+    //[ClientRpc]
+    //private void ChangeColorClientRpc()
     //{
     //    ChangeColor();
     //}
 
-    [ClientRpc]
-    private void ChangeColorClientRpc()
-    {
-        ChangeColor();
-    }
+    //private void ChangeColor()
+    //{
+    //    //if (!IsOwner) return;
+    //    var _count = _sprites.Length;
+    //    var _randomColor = Random.ColorHSV(0, 1, 0, 1, 0.5f, 1);
 
-    private void ChangeColor()
-    {
-        //if (!IsOwner) return;
-        var _count = _sprites.Length;
-        var _randomColor = Random.ColorHSV(0, 1, 0, 1, 0.5f, 1);
-
-        for (int i = 0; i < _count; i++)
-        {
-            _sprites[i].color = _randomColor;
-        }
-    }
+    //    for (int i = 0; i < _count; i++)
+    //    {
+    //        _sprites[i].color = _randomColor;
+    //    }
+    //}
 }
