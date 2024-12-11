36f19bb02879a55db2b053fa351afe7392445ca4
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-12-11 18:20:26 -0300

    feat: color randomizer finally added.

--- GDTV_Multiplayer_Course_Project/Assets/Scripts/VFX/ColorRandomizer.cs
+++ GDTV_Multiplayer_Course_Project/Assets/Scripts/VFX/ColorRandomizer.cs
@@ -7,26 +7,15 @@ public class ColorRandomizer : NetworkBehaviour
 {
     [SerializeField] SpriteRenderer[] _sprites = null;
 
+    private NetworkVariable<Color> _color = new();
+
     public override void OnNetworkSpawn()
     {
-        base.OnNetworkSpawn();
-        SendColorRpc();
-    }
+        if (IsServer)
+            _color.Value = GetRandomColor();
 
-    public override void OnNetworkDespawn()
-    {
-        base.OnNetworkDespawn();
-    }
-
-    private void SendColorRpc()
-    {
-        if (!IsClient) return;
-        if (!IsOwner) return;
-
-        var _color = GetRandomColor();
-        SetSprites(_color);
-        SetSprites_ServerRpc(_color);
-        SetSprites_ClientRpc(_color);
+        if (IsClient)
+            SetSpritesColor(_color.Value);
     }
 
     private Color GetRandomColor()
@@ -34,7 +23,7 @@ public class ColorRandomizer : NetworkBehaviour
         return Random.ColorHSV(0, 1, 0.4f, 0.8f, 0.6f, 1, 1, 1);
     }
 
-    private void SetSprites(Color _color)
+    private void SetSpritesColor(Color _color)
     {
         var _count = _sprites.Length;
 
@@ -42,17 +31,5 @@ public class ColorRandomizer : NetworkBehaviour
         {
             _sprites[i].color = _color;
         }
-    }
-
-    [ServerRpc]
-    private void SetSprites_ServerRpc(Color _color)
-    {
-        SetSprites(_color);
-    }
-
-    [ClientRpc]
-    private void SetSprites_ClientRpc(Color _color)
-    {
-        SetSprites(_color);
     }
 }
