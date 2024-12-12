1ef3fb091ad6f4832fc77a135dcab6745ea5226a
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-12-12 19:46:33 -0300

    feat: coin wallet component added.

---
+++ GDTV_Multiplayer_Course_Project/Assets/Scripts/Coins/CoinWallet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinWallet : NetworkBehaviour
+{
+    public NetworkVariable<int> _totalCoins = new();
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.TryGetComponent(out Coin _coin))
+        {
+            var _coinValue = _coin.Collect();
+
+            if (IsServer)
+            {
+                _totalCoins.Value += _coinValue;
+            }
+        }
+    }
+}
