e003855ef3522a62458bd45252100455e594f6bf
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2024-12-27 20:32:16 -0300

    feat: lobby creation added.

--- GDTV_Multiplayer_Course_Project/Assets/Scripts/Networking/HostGameManager.cs
+++ GDTV_Multiplayer_Course_Project/Assets/Scripts/Networking/HostGameManager.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -12,8 +14,9 @@ public class HostGameManager
 {
     private Allocation _allocation = null;
     private string _joinCode = null;
+    private string _lobbyId = null;
 
-    private const int MAX_ALLOCATIONS = 20;
+    private const int MAX_CONNECTIONS = 20;
     public const string CONNECTION_TYPE = "dtls";
     private const string GAME_SCENE_NAME = "Game";
 
@@ -21,7 +24,7 @@ public class HostGameManager
     {
         try
         {
-            _allocation = await Relay.Instance.CreateAllocationAsync(MAX_ALLOCATIONS);
+            _allocation = await Relay.Instance.CreateAllocationAsync(MAX_CONNECTIONS);
         }
         catch (System.Exception _exception)
         {
@@ -38,11 +41,47 @@ public class HostGameManager
             throw _exception;
         }
 
+        try
+        {
+            var _lobbyOptions = new CreateLobbyOptions();
+            _lobbyOptions.IsPrivate = false;
+            _lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject
+                    (
+                    visibility: DataObject.VisibilityOptions.Member,
+                    value : _joinCode
+                    )
+                }
+            };
+
+            var _lobby = await Lobbies.Instance.CreateLobbyAsync("My Lobby", MAX_CONNECTIONS, _lobbyOptions);
+            _lobbyId = _lobby.Id;
+
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15));
+        }
+        catch (LobbyServiceException _lobbyException)
+        {
+            throw _lobbyException;
+        }
+
         var _transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         var _relayServerData = new RelayServerData(_allocation, CONNECTION_TYPE);
         _transport.SetRelayServerData(_relayServerData);
 
         NetworkManager.Singleton.StartHost();
         NetworkManager.Singleton.SceneManager.LoadScene(GAME_SCENE_NAME, UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+
+    private IEnumerator HeartBeatLobby(float _waitTimeInSeconds)
+    {
+        var _waitTime = new WaitForSecondsRealtime(_waitTimeInSeconds);
+
+        while (true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(_lobbyId);
+            yield return _waitTime;
+        }
     }
 }
