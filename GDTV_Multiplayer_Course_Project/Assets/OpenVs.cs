a2fca9bbb2d116ecbaf2b2952c1808e32fd7405b
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-01-27 18:44:35 -0300

    test: open vs cleaned.

--- GDTV_Multiplayer_Course_Project/Assets/OpenVs.cs
+++ GDTV_Multiplayer_Course_Project/Assets/OpenVs.cs
@@ -1,15 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class OpenVs : MonoBehaviour
 {
-    private void Start()
-    {
-        if (TryGetComponent(out Transform _t))
-        {
-            _t.SetPositionAndRotation(Vector3.right, Quaternion.identity);
-        }
-    }
 }
