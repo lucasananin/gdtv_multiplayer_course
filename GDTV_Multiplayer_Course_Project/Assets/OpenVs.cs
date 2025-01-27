980eee1ef3143eee907ec88897119ffd98f4395c
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-01-27 18:42:52 -0300

    test: new pc script test.

--- GDTV_Multiplayer_Course_Project/Assets/OpenVs.cs
+++ GDTV_Multiplayer_Course_Project/Assets/OpenVs.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class OpenVs : MonoBehaviour
 {
+    private void Start()
+    {
+        if (TryGetComponent(out Transform _t))
+        {
+            _t.SetPositionAndRotation(Vector3.right, Quaternion.identity);
+        }
+    }
 }
